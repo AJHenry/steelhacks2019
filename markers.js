82744be6cab889abf5036cb376343ce7fca2ef63
Nicholas Smider <njs70@pitt.edu>
2019-03-02 13:19:12 -0500

    add markers

---
+++ markers.js
@@ -0,0 +1,44 @@
+export default [
+    {
+        latitude: 40.429330,
+        longitude: -79.993650,
+        title: 'Appliance Warehouse',
+        subtitle: '20 S 6th St Rear, Pittsburgh, PA 15203'
+    },
+    {
+        latitude: 40.425910,
+        longitude: -79.973580,
+        title: 'Evolution E-Cycling LLC',
+        subtitle: '2235 Mary St. Pittsburgh, PA 15203'
+    },
+    {
+        latitude: 40.418930,
+        longitude: -79.606520,
+        title: 'eLoop LLC Regional Recycling Center',
+        subtitle: '2010 Borland Farm Rd, Export, PA'
+    },
+    {
+        latitude: 41.505170,
+        longitude: -80.188640,
+        title: 'ECS&R',
+        subtitle: '3237 US Highway 19, Cochranton, PA'
+    },
+    {
+        latitude: 40.412740,
+        longitude: -79.903480,
+        title: 'Best Buy',
+        subtitle: '680 Waterfront Drive, E Munhall, PA 15120'
+    },
+    {
+        latitude: 40.523770,
+        longitude: -80.006570,
+        title: 'Best Buy',
+        subtitle: '4801 McKnight Rd, Pittsburgh, PA'
+    },
+    {
+        latitude: 40.346410,
+        longitude: -80.044950,
+        title: 'Best Buy',
+        subtitle: '5000 Alicia Drive, Bethel Park, PA'
+    }
+]
